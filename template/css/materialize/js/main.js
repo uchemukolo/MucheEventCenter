00220024682fe41adff244dfa063b025068bd3c4
unknown <muche>
2018-02-16 16:19:23 +0100

    add UI templates

---
+++ template/css/materialize/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
