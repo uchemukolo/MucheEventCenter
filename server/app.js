897cdf584cdb2c9a9fb595ae388c721d21eb9446
unknown <muche>
2018-03-08 15:15:12 +0100

    refactor app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import event from './routes/event';
+import center from './routes/center';
 
-// setup express app
+
+
+// setup/initialize express app
 const app = express();
 
 // log requests to the console
@@ -11,6 +15,10 @@ app.use(logger('dev'));
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/events', event);
+app.use('/api/centers', center);
+
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
